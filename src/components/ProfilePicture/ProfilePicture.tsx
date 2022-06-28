cec82ff9c9e59a4c74b0d1252c11aea1818ea933
Zora Peteri <zorapeteri@gmail.com>
2022-06-28 17:21:18 +0200

    add fallback image for profile picture

--- src/components/ProfilePicture/ProfilePicture.tsx
+++ src/components/ProfilePicture/ProfilePicture.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import className from 'utils/className';
 import globalClassName from 'utils/globalClassName';
 import css from './ProfilePicture.module.css';
@@ -8,16 +8,39 @@ type ProfilePictureProps = {
     clickableProfileLink?: boolean;
 };
 
-const ProfilePicture = ({ image, clickableProfileLink }: ProfilePictureProps) => (
-  <img
-    src={image}
-    alt=""
-    {...className(
-      globalClassName('author-image'),
-      css.profilePicture,
-      clickableProfileLink && css.clickable,
-    )}
-  />
-);
+const fallback = 'https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png';
+
+const ProfilePicture = ({ image, clickableProfileLink }: ProfilePictureProps) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => setImageLoaded(false), [image]);
+
+  return (
+    <>
+      <img
+        src={image}
+        alt=""
+        {...className(
+          globalClassName('author-image'),
+          css.profilePicture,
+          clickableProfileLink && css.clickable,
+          !imageLoaded && css.hide,
+        )}
+        onError={() => setImageLoaded(false)}
+        onLoad={() => setImageLoaded(true)}
+      />
+      <img
+        src={fallback}
+        alt=""
+        {...className(
+          globalClassName('author-image'),
+          css.profilePicture,
+          clickableProfileLink && css.clickable,
+          imageLoaded && css.hide,
+        )}
+      />
+    </>
+  );
+};
 
 export default ProfilePicture;
